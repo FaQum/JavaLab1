dfb012eb46bbca70054a0f30d712e1a9e11d9836
Kamil Świerczyński <kspuck3@gmail.com>
2021-02-28 13:33:01 +0100

    add phone objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,5 +13,12 @@ public class Main {
 	    Animal tiger = new Animal("Maniek", "Tygrys bengalski", "Samiec");
 
 	    System.out.println(tiger.spieces);
+
+	    Phone phone1 = new Phone("Apple", "12 Pro Max", "5G");
+	    Phone phone2 = new Phone("Samsung", "Galaxy S10", "4G LTE");
+
+	    System.out.println(phone1.brand +" "+ phone1.model);
+	    System.out.println(phone2.brand +" "+ phone2.model);
+
     }
 }
--- src/com/company/Phone.java
+++ src/com/company/Phone.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Phone {
+    String brand;
+    String model;
+    String communication;
+
+    public Phone(String brand, String model, String communication) {
+        this.brand = brand;
+        this.model = model;
+        this.communication = communication;
+    }
 }
