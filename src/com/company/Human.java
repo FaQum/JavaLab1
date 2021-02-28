6330adf9839563acd30525d73ea0bec1f7eca7d2
Kamil Świerczyński <kspuck3@gmail.com>
2021-02-28 13:25:29 +0100

    add human object

---
+++ src/com/company/Human.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Human {
+    String firstName;
+    String lastName;
+    String position;
+    double salary;
+
+    public Human(String firstName, String lastName, String position, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.salary = salary;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Hello world");
+
+	    Human human1 = new Human("Jan", "Nowak", "Java Developer", 1200);
+	    Human human2 = new Human("Janusz", "Kowalski", "PHP Developer", 5000);
+
+	    System.out.println(human1.firstName);
     }
 }
