1b6655c8e8991414cd1f2470db4084820a04883a
Kamil Świerczyński <kspuck3@gmail.com>
2021-02-28 13:55:01 +0100

    if statement tests

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -4,10 +4,12 @@ public class Animal {
     String name;
     String spieces;
     String gender;
+    double weight;
 
-    public Animal(String name, String spieces, String gender) {
+    public Animal(String name, String spieces, String gender, double weight) {
         this.name = name;
         this.spieces = spieces;
         this.gender = gender;
+        this.weight = weight;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,8 @@ public class Main {
 
 	    System.out.println(human1.firstName);
 
-	    Animal tiger = new Animal("Maniek", "Tygrys bengalski", "Samiec");
+	    Animal tiger = new Animal("Maniek", "Tygrys bengalski", "Samiec", 200);
+	    Animal llama = new Animal("Lamcia", "Lama andyjska", "Samica", 78);
 
 	    System.out.println(tiger.spieces);
 
@@ -20,5 +21,14 @@ public class Main {
 	    System.out.println(phone1.brand +" "+ phone1.model);
 	    System.out.println(phone2.brand +" "+ phone2.model);
 
-    }
+	    if(tiger.weight > llama.weight)
+	    	System.out.println("Tygrys jest cięższy od lamy i waży " + tiger.weight + " kg");
+		 else
+		 	System.out.println("Lama jest cięższa od tygrysa i waży" + llama.weight + " kg");
+
+		if(human1.salary > human2.salary)
+			System.out.println(human1.firstName + " zarabia więcej od " + human2.firstName);
+		else
+			System.out.println(human1.firstName + " zarabia mniej niż " + human2.firstName);
+	}
 }
